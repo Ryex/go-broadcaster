0f57e3851864b06d54191f68d80c574ceeb1dea0
Benjamin Powers <ryexander@gmail.com>
2018-06-29 12:13:52 -0600

    remove atempt to print out composer

--- shared/models/track.go
+++ shared/models/track.go
@@ -20,6 +20,6 @@ type Track struct {
 }
 
 func (t Track) String() string {
-	return fmt.Sprintf("{ Title: %v, Album: %v, Composer: %v, Genre: %v, Year: %v, Length: %v, Bitrate: %v, Channels: %v, Samplerate: %v, Path: %v}",
-		t.Title, t.Album, t.Composer, t.Genre, t.Year, t.Length, t.Bitrate, t.Channels, t.Samplerate, t.Path)
+	return fmt.Sprintf("{ Title: %v, Album: %v, Genre: %v, Year: %v, Length: %v, Bitrate: %v, Channels: %v, Samplerate: %v, Path: %v}",
+		t.Title, t.Album, t.Genre, t.Year, t.Length, t.Bitrate, t.Channels, t.Samplerate, t.Path)
 }
